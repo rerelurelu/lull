441bc34676b0a184e204650d1a7ebcdf12dfc818
rerelurelu <awaawapurin@gmail.com>
2024-11-16 17:10:07 +0900

    feat: Add OGP

---
+++ src/app/posts/[page]/opengraph-image.tsx
@@ -0,0 +1,25 @@
+import { OgpWrapper } from '@/components/Ogp/OgpWrapper'
+import { OGP_FONT } from '@/constants'
+import { OGP_IMAGE_SIZE } from '@/constants/index'
+import { fetchGoogleFonts } from '@/services/fonts'
+import { ImageResponse } from 'next/og'
+
+export const runtime = 'edge'
+export const alt = 'Reluの投稿記事一覧'
+export const contentType = 'image/png'
+
+export default async function Image() {
+  const font = await fetchGoogleFonts(OGP_FONT)
+
+  return new ImageResponse(<OgpWrapper>Reluの投稿記事一覧</OgpWrapper>, {
+    ...OGP_IMAGE_SIZE,
+    fonts: [
+      {
+        name: OGP_FONT,
+        data: font,
+        style: 'normal',
+        weight: 100,
+      },
+    ],
+  })
+}
---
+++ src/components/Ogp/OgpWrapper.tsx
@@ -0,0 +1,65 @@
+import type { FC, PropsWithChildren } from 'react'
+
+export const OgpWrapper: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <div
+      style={{
+        fontSize: 72,
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        background: 'linear-gradient(to right,  #00f1f9 0%, #cb33f4 100%)',
+        padding: '24px',
+      }}
+    >
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          color: 'white',
+          background: '#1a1e2e',
+          display: 'flex',
+          justifyContent: 'space-between',
+          flexDirection: 'column',
+          borderRadius: 12,
+          padding: '40px',
+        }}
+      >
+        {children}
+        <div style={{ width: '100%', display: 'flex' }}>
+          <div
+            style={{
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'flex-start',
+              alignItems: 'center',
+              fontSize: 56,
+              gap: '20px',
+            }}
+          >
+            <div
+              style={{
+                display: 'flex',
+                borderRadius: '9999px',
+                overflow: 'hidden',
+                boxShadow: '0 0 0 3px #999eef',
+                alignItems: 'center',
+                marginLeft: '8px',
+              }}
+            >
+              <img
+                src='https://avatars.githubusercontent.com/u/43092452?v=4'
+                alt='Relu`s Icon'
+                width={56}
+                height={56}
+              />
+            </div>
+            <span style={{ paddingBottom: '5px' }}>Relu</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
