fec6b220f63ce9d917ef29b9ce9d488104f363b5
rerelurelu <awaawapurin@gmail.com>
2025-09-20 13:43:25 +0900

    chore: update Next.js configuration to enable typed routes and experimental React compiler; bump Next.js version to 15.5.3 and add babel-plugin-react-compiler

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: process.cwd(),
   },
+  typedRoutes: true,
+  experimental: {
+    reactCompiler: true,
+  },
 }
 
 export default nextConfig
